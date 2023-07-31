f74a4f09b2772afd23453a234ee91737d39d3f9b
Oséias <oseas-ramos123@hotmail.com>
2023-07-31 08:40:00 -0300

    Adiciona novas funcionalidades

--- Js.js
+++ Js.js
@@ -2,3 +2,19 @@ function toggleMenu() {
     const menu = document.getElementById("menu");
     menu.classList.toggle("active");
 }
+
+
+async function shortUrl(){
+    const url = document.getElementById("long-url").value;
+    const response = await fetch("http://localhost:8080/shortner?url=" + url);
+   
+    if (response.ok) {
+        const shortUrlResponse = await response.json();
+        const shortInputUrl = document.getElementById("short-url");
+        shortInputUrl.classList.remove("d-none");
+        shortInputUrl.value = "http://localhost:8080/" + shortUrlResponse.shortUrl;
+    } else {
+        const shortUrlResponse = await response.json();
+        alert(shortUrlResponse + " Atenção!!: URL invalida"); // Exibe a mensagem de erro no front-end
+    }
+}
--- JsInfo.js
+++ JsInfo.js
@@ -1,4 +1,25 @@
 
-function voltarPagina() {
+function backPage() {
     window.history.back();
 }
+
+
+async function clickUrl(){
+    const url = document.getElementById("shortner-url").value;
+
+    let parsedUrl = new URL(url).pathname.replace("/", "")
+
+
+    const response = await fetch("http://localhost:8080/getCount?shortUrl=" + parsedUrl);
+    const conutUrlResponse = await response.json();
+    const countIputUrl = document.getElementById("click-url");
+ 
+ 
+    countIputUrl.classList.remove("d-none");
+    countIputUrl.value = conutUrlResponse.urlAccessCount;
+    
+
+    
+}
+
+
